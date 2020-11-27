6ddc1128c828c23471ddeb9e24442b7748a4cd31
Zawar Qureshi <qureshizawar@gmail.com>
2020-11-27 12:19:26 +0000

    ui/code improvements

--- js/depthDemo.js
+++ js/depthDemo.js
@@ -10,52 +10,59 @@ let model_depth_decoder;
 
 // H x W
 const input_res_lookup = {
-  outdoors1_low: [128, 320],
-  outdoors1_mid: [192, 320],
-  outdoors1_high: [192, 640],
-  outdoors2_low: [160, 320],
-  outdoors2_mid: [256, 480],
-  outdoors2_high: [320, 640],
-  indoors_low: [224, 320],
-  indoors_mid: [320, 448],
-  indoors_high: [416, 544]
+  scene_outdoors1_depth_low: [128, 320],
+  scene_outdoors1_depth_mid: [192, 320],
+  scene_outdoors1_depth_high: [192, 640],
+  scene_outdoors2_depth_low: [160, 320],
+  scene_outdoors2_depth_mid: [256, 480],
+  scene_outdoors2_depth_high: [320, 640],
+  scene_indoors_depth_low: [224, 320],
+  scene_indoors_depth_mid: [320, 448],
+  scene_indoors_depth_high: [416, 544]
 };
 
 const model_lookup = {
-  outdoors1: ['/assets/tfjs_encoder_quant/model.json',
+  scene_outdoors1: ['/assets/tfjs_encoder_quant/model.json',
     '/assets/tfjs_decoder_quant/model.json'
   ],
-  outdoors2: ['/assets/outdoors_tfjs_encoder/model.json',
-    '/assets/outdoors_tfjs_decoder/model.json'
+  // scene_outdoors2: ['/assets/outdoors_tfjs_encoder/model.json',
+  //   '/assets/outdoors_tfjs_decoder/model.json'
+  // ],
+  scene_outdoors2: ['/assets/outdoors_tfjs_encoder_quant/model.json',
+    '/assets/outdoors_tfjs_decoder_quant/model.json'
   ],
-  indoors: ['/assets/indoors_tfjs_encoder/model.json',
+  scene_indoors: ['/assets/indoors_tfjs_encoder/model.json',
     '/assets/indoors_tfjs_decoder/model.json'
   ]
+  // scene_indoors: ['/assets/indoors_tfjs_encoder_quant/model.json',
+  //   '/assets/indoors_tfjs_decoder_quant/model.json'
+  // ]
+
 };
 
 const norm_lookup = {
-  outdoors1: [
+  scene_outdoors1: [
     [0, 0, 0],
     [1, 1, 1]
   ],
-  outdoors2: [
+  scene_outdoors2: [
     [0.485, 0.456, 0.406],
     [0.229, 0.224, 0.225]
   ],
-  indoors: [
+  scene_indoors: [
     [0.485, 0.456, 0.406],
     [0.229, 0.224, 0.225]
   ]
 }
 
 const out_idx = {
-  outdoors1: 3,
-  outdoors2: 0,
-  indoors: 0
+  scene_outdoors1: 3,
+  scene_outdoors2: 0,
+  scene_indoors: 0
 }
 
-var curr_model = "outdoors2"
-var curr_res = "mid"
+var curr_model = "scene_outdoors2"
+var curr_res = "depth_mid"
 
 var Depth_IMAGE_HEIGHT = 192;
 var Depth_IMAGE_WIDTH = 320;
@@ -71,43 +78,53 @@ const depth_medium = document.getElementById('depth_medium');
 const depth_high = document.getElementById('depth_high');
 
 if (!(is_touch_device()) && !(window.matchMedia('(max-device-width: 960px)').matches)) {
-  curr_res = "high"
+  curr_res = "depth_high"
   dropdown_depth_qual.textContent = depth_high.textContent;
+  Depth_IMAGE_WIDTH = input_res_lookup[curr_model.concat("_").concat(curr_res)][1]
+  Depth_IMAGE_HEIGHT = input_res_lookup[curr_model.concat("_").concat(curr_res)][0]
 }
 
 document.getElementById("files").addEventListener("change", function(evt) {
+  status_depth.textContent = 'Status: Loading...';
   file_infer(evt.target.files[0], document.getElementById('inpimg'),
     status_depth, Depth_Demo);
+
+  // status_depth.textContent = "Status: Done!";
 });
 document.getElementById("depth_files_btn").addEventListener("click", function(evt) {
   file = document.getElementById("files").files[0];
   if (file == null) {
     status_depth.textContent = 'Status: File not found';
   } else {
+    status_depth.textContent = 'Status: Loading...';
     file_infer(file, document.getElementById('inpimg'),
       status_depth, Depth_Demo);
+    // status_depth.textContent = "Status: Done!";
   }
 });
 document.getElementById("select_files_btn").onclick = function() {
   // event.preventDefault();
   // console.log("btn pressed!")
-  status_depth.textContent = 'Status: Fetching image...';
+  status_depth.textContent = 'Status: Loading...';
   img_in = document.getElementById('inpimg')
   Depth_Demo(img_in);
+  // status_depth.textContent = "Status: Done!";
 }
-document.getElementById("depth_files_btn").addEventListener("click", function(evt) {
-  file = document.getElementById("files").files[0];
-  if (file == null) {
-    status_depth.textContent = 'Status: File not found';
-  } else {
-    file_infer(file, document.getElementById('inpimg'),
-      status_depth, Depth_Demo);
-  }
-});
+// document.getElementById("depth_files_btn").addEventListener("click", function(evt) {
+//   file = document.getElementById("files").files[0];
+//   if (file == null) {
+//     status_depth.textContent = 'Status: File not found';
+//   } else {
+//     file_infer(file, document.getElementById('inpimg'),
+//       status_depth, Depth_Demo);
+//   }
+// });
 
 document.getElementById("btn").addEventListener("click", function(evt) {
+  status_depth.textContent = 'Status: Loading...';
   url_infer(document.getElementById('imagename'), document.getElementById('inpimg'),
     status_depth, Depth_Demo);
+  // status_depth.textContent = "Status: Done!";
 });
 
 function custom_mgrid(rows, cols) {
@@ -122,6 +139,12 @@ const update_model = async (name) => {
   // console.log(model_lookup[name][0])
   model_depth_encoder = await tf.loadLayersModel(model_lookup[name][0]);
   model_depth_decoder = await tf.loadLayersModel(model_lookup[name][1]);
+
+  const dummy = tf.tidy(() => {
+    const dummy_feat = model_depth_encoder.predict(tf.ones([1, 3, Depth_IMAGE_HEIGHT, Depth_IMAGE_WIDTH]))
+    const dummy = model_depth_decoder.predict(dummy_feat);
+    return 0
+  });
 
 };
 
@@ -148,6 +171,7 @@ const DepthWarmup = async () => {
     }
   }
 
+  // status_depth.textContent = "Status: Done!";
   // document.getElementById('file-container').style.display = '';
 };
 
@@ -156,30 +180,34 @@ const Depth_Demo = async (imElement) => {
   //var t0 = performance.now();
   // var depth_time = 0;
 
-  status_depth.textContent = 'Status: Loading image into model...';
+  // status_depth.textContent = 'Status: Loading image into model...';
+  status_depth.textContent = 'Status: Loading...';
 
   output_WIDTH = imElement.clientWidth;
   output_HEIGHT = imElement.clientHeight;
 
   // console.log(curr_model.concat("_").concat(curr_res))
 
-  Depth_IMAGE_WIDTH = input_res_lookup[curr_model.concat("_").concat(curr_res)][1]
-  Depth_IMAGE_HEIGHT = input_res_lookup[curr_model.concat("_").concat(curr_res)][0]
+  // Depth_IMAGE_WIDTH = input_res_lookup[curr_model.concat("_").concat(curr_res)][1]
+  // Depth_IMAGE_HEIGHT = input_res_lookup[curr_model.concat("_").concat(curr_res)][0]
 
   // console.log(Depth_IMAGE_WIDTH)
   // console.log(Depth_IMAGE_HEIGHT)
 
   in_img = tf.browser.fromPixels(imElement).toFloat();
 
-  const depthMask = tf.tidy(() => {
+  const depthMask_resized = tf.tidy(() => {
 
     const img = tf.image.resizeBilinear(in_img,
       [Depth_IMAGE_HEIGHT, Depth_IMAGE_WIDTH]);
 
     const scale = tf.scalar(255.);
 
-    const mean = tf.tensor3d(norm_lookup[curr_model][0], [1, 1, 3]);
-    const std = tf.tensor3d(norm_lookup[curr_model][1], [1, 1, 3]);
+    // const mean = tf.tensor3d(norm_lookup[curr_model][0], [1, 1, 3]);
+    // const std = tf.tensor3d(norm_lookup[curr_model][1], [1, 1, 3]);
+
+    const mean = tf.tensor3d([0.485, 0.456, 0.406], [1, 1, 3]);
+    const std = tf.tensor3d([0.229, 0.224, 0.225], [1, 1, 3]);
 
     const normalised = img.div(scale).sub(mean).div(std);
 
@@ -191,11 +219,13 @@ const Depth_Demo = async (imElement) => {
     const predictions = model_depth_decoder.predict(features);
     //var it1 = performance.now();
     //depth_time = (it1 - it0);
-    // const depthPred = predictions[3].squeeze(0).transpose([1, 2, 0]);
-    const depthPred = predictions[out_idx[curr_model]].squeeze(0).transpose([1, 2, 0]);
+    // const dispPred = predictions[3].squeeze(0).transpose([1, 2, 0]);
+    const dispPred = predictions[0].squeeze(0).transpose([1, 2, 0]);
+    // const dispPred = predictions[out_idx[curr_model]].squeeze(0).transpose([1, 2, 0]);
 
-    return depthPred.sub(depthPred.min()).divNoNan(depthPred.max().sub(depthPred.min()));
+    const dispPred_norm = dispPred.sub(dispPred.min()).divNoNan(dispPred.max().sub(dispPred.min()));
 
+    return tf.image.resizeBilinear(dispPred_norm, [output_HEIGHT, output_WIDTH])
   });
 
   const depthCanvas = document.getElementById('depth');
@@ -204,15 +234,14 @@ const Depth_Demo = async (imElement) => {
 
   //var t1 = performance.now();
   //console.log("Call to depth took " + (t1 - t0) + " milliseconds.");
-
-  const depthMask_resized = tf.image.resizeBilinear(depthMask, [output_HEIGHT, output_WIDTH])
+  //
+  // const depthMask_resized = tf.image.resizeBilinear(depthMask, [output_HEIGHT, output_WIDTH])
 
   img_array = tf.image.resizeBilinear(in_img, [output_HEIGHT, output_WIDTH]).arraySync()
   depth_array = depthMask_resized.arraySync()
 
   await tf.browser.toPixels(depthMask_resized, depthCanvas);
 
-  status_depth.textContent = "Status: Done!";
   //status_depth.textContent = "Status: Done! inference took " + (depth_time.toFixed(1)) + " milliseconds.";
   //console.log("before: ", tf.memory());
 
@@ -223,56 +252,82 @@ const Depth_Demo = async (imElement) => {
   createPointCloud(xy[0].arraySync(), xy[1].arraySync(), depth_array, img_array);
 
   in_img.dispose();
-  depthMask.dispose();
+  // depthMask.dispose();
+  xy[0].dispose();
+  xy[1].dispose();
   depthMask_resized.dispose();
+  status_depth.textContent = "Status: Done!";
 };
 
 
 // model type settings
 
-scene_indoors.onclick = function() {
+function set_model(model) {
   event.preventDefault();
-  //console.log("btn pressed!")
-  curr_model = "indoors"
+  curr_model = model
   update_model(curr_model);
-  dropdown_scene.textContent = scene_indoors.textContent;
+  Depth_IMAGE_WIDTH = input_res_lookup[curr_model.concat("_").concat(curr_res)][1]
+  Depth_IMAGE_HEIGHT = input_res_lookup[curr_model.concat("_").concat(curr_res)][0]
+
+  dropdown_scene.textContent = document.getElementById(model).textContent;
 }
-scene_outdoors1.onclick = function() {
-  event.preventDefault();
-  //console.log("btn pressed!")
-  curr_model = "outdoors1"
-  update_model(curr_model);
-  dropdown_scene.textContent = scene_outdoors1.textContent;
-}
-scene_outdoors2.onclick = function() {
-  event.preventDefault();
-  //console.log("btn pressed!")
-  curr_model = "outdoors2"
-  update_model(curr_model);
-  dropdown_scene.textContent = scene_outdoors2.textContent;
-}
+//
+// scene_indoors.onclick = function() {
+//   event.preventDefault();
+//   //console.log("btn pressed!")
+//   curr_model = "indoors"
+//   update_model(curr_model);
+//   dropdown_scene.textContent = scene_indoors.textContent;
+// }
+// scene_outdoors1.onclick = function() {
+//   event.preventDefault();
+//   //console.log("btn pressed!")
+//   curr_model = "outdoors1"
+//   update_model(curr_model);
+//   dropdown_scene.textContent = scene_outdoors1.textContent;
+// }
+// scene_outdoors2.onclick = function() {
+//   event.preventDefault();
+//   //console.log("btn pressed!")
+//   curr_model = "outdoors2"
+//   update_model(curr_model);
+//   dropdown_scene.textContent = scene_outdoors2.textContent;
+// }
 
 // input resolution settings
 
-depth_low.onclick = function() {
+function set_res(res) {
   event.preventDefault();
-  //console.log("btn pressed!")
-  curr_res = "low";
+  curr_res = res;
+  Depth_IMAGE_WIDTH = input_res_lookup[curr_model.concat("_").concat(curr_res)][1]
+  Depth_IMAGE_HEIGHT = input_res_lookup[curr_model.concat("_").concat(curr_res)][0]
+  dropdown_depth_qual.textContent = document.getElementById(res).textContent;
+}
 
-  dropdown_depth_qual.textContent = depth_low.textContent;
-}
-depth_medium.onclick = function() {
-  event.preventDefault();
-  //console.log("btn pressed!")
-  curr_res = "mid";
-  dropdown_depth_qual.textContent = depth_medium.textContent;
-}
-depth_high.onclick = function() {
-  event.preventDefault();
-  //console.log("btn pressed!")
-  curr_res = "high";
-  dropdown_depth_qual.textContent = depth_high.textContent;
-}
+// depth_low.onclick = function() {
+//   event.preventDefault();
+//   //console.log("btn pressed!")
+//   curr_res = "low";
+//   Depth_IMAGE_WIDTH = input_res_lookup[curr_model.concat("_").concat(curr_res)][1]
+//   Depth_IMAGE_HEIGHT = input_res_lookup[curr_model.concat("_").concat(curr_res)][0]
+//   dropdown_depth_qual.textContent = depth_low.textContent;
+// }
+// depth_medium.onclick = function() {
+//   event.preventDefault();
+//   //console.log("btn pressed!")
+//   curr_res = "mid";
+//   Depth_IMAGE_WIDTH = input_res_lookup[curr_model.concat("_").concat(curr_res)][1]
+//   Depth_IMAGE_HEIGHT = input_res_lookup[curr_model.concat("_").concat(curr_res)][0]
+//   dropdown_depth_qual.textContent = depth_medium.textContent;
+// }
+// depth_high.onclick = function() {
+//   event.preventDefault();
+//   //console.log("btn pressed!")
+//   curr_res = "high";
+//   Depth_IMAGE_WIDTH = input_res_lookup[curr_model.concat("_").concat(curr_res)][1]
+//   Depth_IMAGE_HEIGHT = input_res_lookup[curr_model.concat("_").concat(curr_res)][0]
+//   dropdown_depth_qual.textContent = depth_high.textContent;
+// }
 
 const inpimg_lookup = {
   inp_outdoors: "assets/demo_images/inp_outdoors.png",
